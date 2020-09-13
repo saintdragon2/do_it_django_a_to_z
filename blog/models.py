eaa213a7b1547260646e5d20a2a411205c13caeb
SaintDragon2 <saintdragon2@gmail.com>
2020-09-13 22:48:43 +0900

    Post에 file_upload 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author: 추후 작성 예정
