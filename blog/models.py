5b93954b7cec46cbb34b05255b7f6a97ac91695a
SaintDragon2 <saintdragon2@gmail.com>
2020-09-13 23:25:17 +0900

    첨부파일 있을 경우 post detail 페이지에 다운로드 버튼 출력

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 
 class Post(models.Model):
@@ -17,3 +18,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
