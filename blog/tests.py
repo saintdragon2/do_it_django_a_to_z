3ed62f9bd66ea24877c683ca87facd8ef9ef1631
SaintDragon2 <saintdragon2@gmail.com>
2020-09-19 14:30:25 +0900

    post가 하나도 없는 경우 처리하기; blog_list 페이지 테스트 코드 만들기

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
-# Create your tests here.
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual(soup.title.text, 'Blog')
+
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        self.assertEqual(Post.objects.count(), 0)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다', main_area.text)
+
+        post_001 = Post.objects.create(
+            title='첫번째 포스트입니다.',
+            content='Hello World. We are the world.',
+        )
+
+        post_002 = Post.objects.create(
+            title='두번째 포스트입니다.',
+            content='1등이 전부는 아니잖아요?',
+        )
+
+        self.assertEqual(Post.objects.count(), 2)
+
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual(response.status_code, 200)
+        main_area = soup.find('div', id='main-area')
+
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
