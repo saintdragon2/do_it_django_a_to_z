dee1a8a4623fb6a158d6e32ac615068123949a01
SaintDragon2 <saintdragon2@gmail.com>
2020-09-20 14:51:28 +0900

    on_delete=models.CASCADE가 되도록 ‘author’필드를 추가함

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 
@@ -12,10 +13,11 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author: 추후 작성 예정
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
