b3e886d9eab433edab7e050bfe7a458fccadd45a
SaintDragon2 <saintdragon2@gmail.com>
2020-09-20 15:34:39 +0900

    작성자 user가 삭제되었을 때, Post의 author는 None으로 변경

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
