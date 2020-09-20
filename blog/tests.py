0a652dacea1832bedec48820e7c1f536e21883a5
SaintDragon2 <saintdragon2@gmail.com>
2020-09-20 14:39:04 +0900

    include를 이용해 base.html에서 navbar와 footer 분리하기

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         response = self.client.get('/blog/')
 
@@ -16,9 +33,7 @@ class TestView(TestCase):
 
         self.assertEqual(soup.title.text, 'Blog')
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
@@ -47,29 +62,21 @@ class TestView(TestCase):
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
     def test_post_detail(self):
-        # 0.   Post가 하나 있다.
         post_001 = Post.objects.create(
             title='첫번째 포스트입니다.',
             content='Hello World. We are the world.',
         )
-        # 0.1  그 포스트의 url은 'blog/1/' 이다.
+
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
-        # 1.   첫 번째 post의 detail 페이지 테스트
-        # 1.1  첫 번째 post url로 접근하면 정상적으로 작동한다. (status code: 200)
         response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # 1.2  post_list 페이지와 똑같은 네비게이션 바가 있다.
-        navbar = soup.nav  # beautifulsoup를 이용하면 간단히 페이지의 태그 요소에 접근이 가능합니다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
-        # 1.3  첫 번째 post의 title이 브라우저 탭에 표기되는 페이지 title에 있다.
         self.assertIn(post_001.title, soup.title.text)
 
-        # 1.4  첫 번째 post의 title이 post-area에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
@@ -77,5 +84,4 @@ class TestView(TestCase):
         # 1.5  첫 번째 post의 작성자(author)가 post-area에 있다.
         # 아직 작성 불가
 
-        # 1.6  첫 번째 post의 content가 post-area에 있다.
         self.assertIn(post_001.content, post_area.text)
