c792dd63ff6751f132b8d49a63233e696ddd6234
SaintDragon2 <saintdragon2@gmail.com>
2020-09-30 23:52:43 +0900

    Comment form으로 post detail 페이지에서 댓글 작성하기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/new_comment/', views.new_comment),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     # path('<int:pk>/', views.single_post_page),
