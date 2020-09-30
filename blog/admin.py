9390a5a6afbbe90ad99a73cff57dccd2be27870b
SaintDragon2 <saintdragon2@gmail.com>
2020-09-30 23:22:10 +0900

    Comment 모델 만들고 Post Detail 페이지에 적용

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 admin.site.register(Post, MarkdownxModelAdmin)
-
+admin.site.register(Comment)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
--- blog/models.py
+++ blog/models.py
@@ -61,3 +61,17 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
