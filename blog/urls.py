03331b10c26e5bd64e9eb99f32a7a4b65a85ee34
SaintDragon2 <saintdragon2@gmail.com>
2020-10-01 12:25:53 +0900

    Comment 삭제 기능 구현

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('delete_comment/<int:pk>/', views.delete_comment),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
