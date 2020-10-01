6c2e0c5d235a0316ce4684b22e07eeae8dd7eaed
SaintDragon2 <saintdragon2@gmail.com>
2020-10-01 15:36:13 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -75,3 +75,8 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1/2b806f019489cfad/svg/{self.author.email}'
