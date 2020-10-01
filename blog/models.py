0f26d137a6e4f4b39642218d1c143537640b6468
SaintDragon2 <saintdragon2@gmail.com>
2020-10-01 16:39:30 +0900

    대문 페이지 (landing page) 수정

--- blog/models.py
+++ blog/models.py
@@ -61,6 +61,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1/2b806f019489cfad/svg/{self.author.email}'
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
